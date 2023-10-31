5cf579c0a84bb59c9b378d8296790056775d7890
Breno Alvarenga <brenoalvarenga12@hotmail.com>
2023-10-30 23:31:41 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,4 +2,19 @@ package boardgame;
 
 public class Piece {
 
+	protected Position position = null;
+	private Board board;
+	
+	public Piece() {
+	}
+	
+	public Piece(Board board) {
+		super();
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
 }
