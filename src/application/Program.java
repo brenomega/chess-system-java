61c9f34442747c3b3ff40c9982eab22f42f54726
Breno Alvarenga <brenoalvarenga12@hotmail.com>
2023-10-31 14:13:26 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import boardgame.Position;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -22,6 +23,10 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
 				System.out.println();
 				System.out.print("Target: ");
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -9,7 +9,6 @@ public class Position {
 	}
 	
 	public Position(int row, int column) {
-		super();
 		this.row = row;
 		this.column = column;
 	}
